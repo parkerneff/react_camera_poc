481746563d321169265f791f6ed70c2a078a42ec
pneff <pneff@unicon.net>
2018-10-02 15:54:38 -0700

    its working

--- App.js
+++ App.js
@@ -7,33 +7,75 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+    AppRegistry,
+    Dimensions,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
+} from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
 
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to the Camera POC!</Text>
-
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <RNCamera
+                    ref={ref => {
+                        this.camera = ref;
+                    }}
+                    style = {styles.preview}
+                    type={RNCamera.Constants.Type.back}
+                    flashMode={RNCamera.Constants.FlashMode.on}
+                    permissionDialogTitle={'Permission to use camera'}
+                    permissionDialogMessage={'We need your permission to use your camera phone'}
+                    onGoogleVisionBarcodesDetected={({ barcodes }) => {
+                        console.log(barcodes)
+                    }}
+                />
+                <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
+                    <TouchableOpacity
+                        onPress={this.takePicture.bind(this)}
+                        style = {styles.capture}
+                    >
+                        <Text style={{fontSize: 14}}> SNAP </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+    takePicture = async function() {
+        if (this.camera) {
+            const options = { quality: 0.5, base64: true };
+            const data = await this.camera.takePictureAsync(options)
+            console.log(data.uri);
+        }
+    };
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
-
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'black'
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20
+    }
 });
+
